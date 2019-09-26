148ac24425fc981f442caa2d16099bf097fab795
Python master <55576353+brinquisruben@users.noreply.github.com>
2019-09-26 09:32:25 +0200

    Add files via upload

---
+++ saludador_2.py
@@ -0,0 +1,7 @@
+def saludador_2():
+    print "hola " #instruccion de salida
+    nombre=raw_input("como te llamas?: ") #instruccion de entrada
+    print "bienvenido a python"+" "+nombre
+    
+
+saludador_2()
