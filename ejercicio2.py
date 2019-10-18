928b880b238af4b45730f9ab948ae9cc169b0709
Python master <55576353+brinquisruben@users.noreply.github.com>
2019-10-18 09:49:30 +0200

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,10 @@
+# -*- coding: cp1252 -*-
+#conversor
+def ejercicio2():
+    euros=input("introduzca € que tienes= ")
+    dollar= 0.9
+    conversion= euros*dollar
+    print euros*dollar," $"
+    
+ejercicio2()
+    
